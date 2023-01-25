6d296ba3a7886840b439f4d25dd5564ed44a0cc2
Vexy-AA <46859934+Vexy-AA@users.noreply.github.com>
2023-01-25 08:55:30 +0300

    Update mavlink-router.sh
    configuration using meson
    building mavlink using ninja

--- mavlink-router.sh
+++ mavlink-router.sh
@@ -4,6 +4,8 @@ set -e
 set -x
 
 sudo apt install autoconf automake libtool -y
+sudo apt install git meson ninja-build pkg-config gcc g++ systemd -y
+sudo pip3 install meson
 
 ## Git clone mavlink-router
 rm -rf ~/mavlink-router
@@ -14,9 +16,9 @@ rm -rf ~/mavlink-router
 ## and build them
 pushd ~/mavlink-router
  git submodule update --init --recursive
- ./autogen.sh && ./configure CFLAGS='-g -O2' --sysconfdir=/etc --localstatedir=/var --libdir=/usr/lib64 --prefix=/usr
- make
- sudo make install
+ meson setup build
+ ninja -C build
+ sudo ninja -C build install
  sudo mkdir -p /etc/mavlink-router
  sudo cp ~/apsync-Kakute/mavlink-router.conf /etc/mavlink-router/main.conf
  sudo systemctl enable mavlink-router.service
